8f19742416e6b8d533d238154eca3caa6208a781
Beula Samuel <092834@bun2092834.local>
2020-04-20 18:45:23 +1200

    react commponents added

---
+++ src/components/imagge/Imagge.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Imagge.css';
+import background from '../../background.jpeg';
+
+class Imagge extends React.Component{
+  render(){
+    const myImage={
+      width: '45%',
+      margin:'auto',
+      display:'block',
+      marginTop: '75px'
+    }
+    return(
+      <div>
+        <img style={myImage} src={background} alt="background"/>
+      </div>
+    )
+  }
+}
+
+export default Imagge;
